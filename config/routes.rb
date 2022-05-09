632059cef90265b0f9549d03c3e044975fbc890c
Ubuntu <ubuntu@ip-172-31-14-221.ec2.internal>
2022-05-09 22:37:42 +0000

    section 5

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+
+  get 'contacts', to: 'pages#contact'
+  get 'pages/about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
+  
+end 
+
