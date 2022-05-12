c21f415492ce93fede5c4d66790139f42921f0cc
Ubuntu <ubuntu@ip-172-31-14-221.ec2.internal>
2022-05-12 15:09:59 +0000

    Fixing Forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'contacts', to: 'pages#contact'
   get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
