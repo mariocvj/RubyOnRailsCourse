72a188ec15440cd83e5fc41692811916dedfcb22
Ubuntu <ubuntu@ip-172-31-14-221.ec2.internal>
2022-05-23 20:12:42 +0000

    setup production for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'https://mariosarticles.herokuapp.com/'}
-  config.action_mailer.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
