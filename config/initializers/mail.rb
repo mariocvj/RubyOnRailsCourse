323d10a60ef0a39b600346261529f270f71ab0dc
Ubuntu <ubuntu@ip-172-31-14-221.ec2.internal>
2022-05-23 19:32:55 +0000

    setup production for heroku

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,8 +4,8 @@ ActionMailer::Base.smtp_settings =
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
+    :user_name => ENV["apikey"],
+    :password => ENV["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"],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
